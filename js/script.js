be11946ea70151fbecb2b60241cdaf035f7a6caf
VladVertehel <vladvertegel@gmail.com>
2023-07-31 19:32:43 +0300

    Burger menu added

--- js/script.js
+++ js/script.js
@@ -17,10 +17,16 @@ for (let elm of elements) {
   observer.observe(elm);
 }
 
-// Slider
-new Swiper(".mySwiper", {
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
+// Burger
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav_links');
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  navMenu.classList.toggle("active");
 });
+
+document.querySelectorAll('.nav_link').forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
